a3ed2349455e85583df8093652a0f2ff32416b12
zhouytl <zhouyangtianli@gmail.com>
2026-06-08 17:29:51 -0500

    add notes

--- Main_preprocess.py
+++ Main_preprocess.py
@@ -5,53 +5,58 @@ Yangtianli Zhou 06/2026
 """
 
 # %% SmKS Intro
-# from smks_utils.visualization_utils import plot_smks_features, plot_SmKS_travel_time_table
-# plot_smks_features(gcarc=120, depth=560)
-# plot_SmKS_travel_time_table(depth=560)
+# this cell can plot the basic feature of SmKS, like arrival, sample depth, etc.
+from smks_utils.visualization_utils import plot_smks_features, plot_SmKS_travel_time_table
+plot_smks_features(gcarc=120, depth=560)
+plot_SmKS_travel_time_table(depth=560)
 
-# %%
-# from smks_utils.data_utils import merge6_np3d_to_np2d
-# from glob import glob
-# evtdir_list = glob('./data/20*')
-
-# for evtdir in evtdir_list:
-#     evtid = evtdir.split('/')[-1]
-#     merge6_np3d_to_np2d(evtid)
-
-# exit()
 
 # %% measure smks solo residuals
+"""
+This cell can measure the SmKS travel time residual and save .pdf file for every single traces. 
+The measured figures are not saved in the github repo due to folder size limitation. 
+Set plot=False if you prefer a fast run without plot. 
+"""
 from smks_utils.measure_smks_utils import measure_merge6_altogether
 from glob import glob
-# evtdir_list = ['./data/20100724_0535', './data/20110101_0957', './data/20200706_2254','./data/20230301_0536']
 evtdir_list = glob('./data/20*')
-#  glob('./data/20110101_0957')
-
-
 for evtdir in evtdir_list:
         evtid = evtdir.split('/')[-1]
         measure_merge6_altogether(evtid=evtid, fname='df_stations_merge6.csv', sample_rate=10, plot=True)
         # out df_measured.csv
 
-
-exit()
 # %% Post Processing
+"""
+This cell can autoselect and cluster traces into groups, 
+and then summarize every group and plot their distance and residual. 
+residualc: individual cross-correlation measurement
+residualp: individual pick arrival measurement
+residual1: waveform simulation (PREM)
+residual2: waveform simulation (PREM + SP12RTS)
+residual3: waveform simulation (PREM + SP12RTS + Torus)
+residual4: waveform simulation (PREM + SP12RTS + Global)
+residual5: waveform simulation (PREM + SP12RTS + subLLSVP)
+"""
 from smks_utils.selection_utils import auto_select
 from smks_utils.selection_utils import cluster_traces
 from smks_utils.selection_utils import sum_groups_merge6
 from smks_utils.visualization_utils import plot_residual_and_rss
 import matplotlib.pyplot as plt
 import pandas as pd
-import glob
+from glob import glob
 
-# evtdir_list = glob('./data/20100724_0535')
-evtdir_list = ['./data/20100724_0535', './data/20110101_0957', './data/20200706_2254','./data/20230301_0536']
+evtdir_list = glob('./data/20*')
+selection_args = [['test1', 3.0, 0.4, 0.6], # my choice
+                  ['test2', 4.0, 0.6, 0.8]]
 
-
-selection_args = [['test1', 2.0, 0.3, 0.5],
-                  ['test2', 3.0, 0.4, 0.6], # my choice
-                  ['test3', 4.0, 0.5, 0.7],
-                  ['test4', 5.0, 0.6, 0.8]]
+"""
+selection are:
+    1. testid
+    2. minimum SmKS SNR
+    3. cross-correlation coefficient between observed and synthetic 200s SmKS signal
+    4. cross-correlation coefficient between observed SKKS and S3KS,
+        same as cross-correlation coefficient between observed and synthetic S3KS.
+"""
 
 for args in selection_args:
     testid = args[0] 
@@ -67,7 +72,7 @@ for args in selection_args:
         # output df_clustered.csv
         sum_groups_merge6(evtid, fname='df_clustered.csv')
         # output df_groups.csv
-    dfs = [pd.read_csv(f) for f in glob.glob("data/20*/df_groups.csv")]
+    dfs = [pd.read_csv(f) for f in glob("data/20*/df_groups.csv")]
     df_all = pd.concat(dfs, ignore_index=True)
     df_all.to_csv('data/df_all_groups.csv')
     print(testid + "result:")
@@ -81,4 +86,3 @@ for args in selection_args:
     plt.tight_layout()
     plt.show()
 
-# %%
